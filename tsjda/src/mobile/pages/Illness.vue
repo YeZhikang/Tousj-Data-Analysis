c704c7753b4ec098456aa9e744e5035714a1f463
Yezhikang <794101456@qq.com>
2020-02-18 13:06:46 +0800

    修改了一些bug和格式规范

--- tsjda/src/mobile/pages/Illness.vue
+++ tsjda/src/mobile/pages/Illness.vue
@@ -35,21 +35,26 @@
               />
             </section>
             <div style="width: 19% ; " >
-              <div v-for="(cate,index) in illCate" :key="index" style="width: 100%;margin-bottom: 10px;text-align: right">
+              <div
+                v-for="(cate,index) in illCate" :key="index"
+                style="width: 100%;margin-bottom: 10px;text-align: right"
+              >
                 <h3 style="font-size: 12px;color:darkgrey">{{cate | getCate}}</h3>
                 <p>{{illData[cate]}}</p>
               </div>
             </div>
           </div>
         </el-card>
-
         <h2 style="font-size: 22px;color: black">Tips <i class="el-icon-tickets"></i></h2>
         <el-card  style="margin:20px 0 40px 0">
           <div style="text-align: left;">
             <div v-for="(remarkTip,index) in remarks" :key="index">
-              <el-alert :closable="false" style="margin: 8px 0" type="warning" v-if="index === 0" show-icon>{{illData[remarkTip]}}</el-alert>
-              <el-alert :closable="false" style="margin: 8px 0" type="info" v-else>{{illData[remarkTip]}}</el-alert>
-
+              <el-alert :closable="false" style="margin: 8px 0" type="warning" v-if="index === 0" show-icon>
+                {{illData[remarkTip]}}
+              </el-alert>
+              <el-alert :closable="false" style="margin: 8px 0" type="info" v-else>
+                {{illData[remarkTip]}}
+              </el-alert>
             </div>
           </div>
         </el-card>
@@ -57,26 +62,20 @@
         <h2 style="font-size: 22px;color: black;margin-top: 20px">Recent News <i class="el-icon-news"></i></h2>
         <el-card style="margin-top: 25px">
           <el-collapse v-model="activeName" >
-            <el-collapse-item v-for="(newReport,index) in news" :title="newReport.title.length>18 ? newReport.title.substr(0,18) : newReport.title" :name="index">
+            <el-collapse-item
+              v-for="(newReport,index) in news"
+              :title="newReport.title.length > 18 ? newReport.title.substr(0,18) : newReport.title" :name="index"
+            >
               <div style="font-size: 12px;text-align: left;margin-bottom: 6px">
-                <span style="font-weight: 600">{{newReport.infoSource}} <span v-if="newReport.provinceName">- {{newReport.provinceName}}</span></span>
+                <span style="font-weight: 600">
+                  {{newReport.infoSource}}
+                  <span v-if="newReport.provinceName">- {{newReport.provinceName}}</span>
+                </span>
                 <span style="color: darkgrey;float: right">{{newReport.pubDateStr}}</span>
               </div>
               <div style="text-align: left">{{newReport.summary}}</div>
             </el-collapse-item>
-<!--          <el-collapse-item title="反馈 Feedback" name="2">-->
-<!--            <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>-->
-<!--            <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>-->
-<!--          </el-collapse-item>-->
-<!--          <el-collapse-item title="效率 Efficiency" name="3">-->
-<!--            <div>简化流程：设计简洁直观的操作流程；</div>-->
-<!--            <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>-->
-<!--            <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>-->
-<!--          </el-collapse-item>-->
-<!--          <el-collapse-item title="可控 Controllability" name="4">-->
-<!--            <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>-->
-<!--            <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>-->
-<!--          </el-collapse-item>-->
+
           </el-collapse>
         </el-card>
         <h4 style="font-weight: 400; text-align: right;margin: 100px 10px 20px 0;color: rgba(44,62,80,0.76)">Made by <br/><i>Wang Yaxuan</i></h4>
@@ -154,23 +153,23 @@
         methods:{
             getData(){
                 this.$axios.get('http://www.dzyong.top:3005/yiqing/area').then(res => {
-                    let illArr = [];
-                    let data = res.data.data;
-                    data.forEach(item => {
-                        if(illArr.length === 0){
-                            illArr.push({
+                    let $_illArr = [];
+                    let $_data = res.data.data;
+                    $_data.forEach(item => {
+                        if($_illArr.length === 0){
+                            $_illArr.push({
                                 name: item.provinceName,
                                 value: item.confirmedCount,
                                 cityLst: [{cityName:item.cityName,number:item.confirmedCount}]
                             })
                         }else {
-                            for (let i = 0; i < illArr.length; i++) {
-                                if (illArr[i].name === item.provinceName) {
-                                    illArr[i].value += item.confirmedCount
-                                    illArr[i].cityLst.push({cityName:item.cityName,number:item.confirmedCount})
+                            for( let i = 0; i < $_illArr.length; i++ ) {
+                                if ($_illArr[i].name === item.provinceName) {
+                                    $_illArr[i].value += item.confirmedCount
+                                    $_illArr[i].cityLst.push({ cityName:item.cityName,number:item.confirmedCount })
                                     break
-                                } else if (i === illArr.length - 1) {
-                                    illArr.push({
+                                } else if (i === $_illArr.length - 1) {
+                                    $_illArr.push({
                                         name: item.provinceName,
                                         value: item.confirmedCount,
                                         cityLst: [{cityName:item.cityName,number:item.confirmedCount}]
@@ -179,15 +178,19 @@
                             }
                         }
                     })
-                    map.series[0].data = illArr
+                    map.series[0].data = $_illArr
                     map.tooltip.formatter = (e, t, n) => {
-                        let hData =
-                            e.data.cityLst.map(item =>
+                        let $_hData = e.data.cityLst.map(item =>
                                 `<div style="font-size: 12px;text-align: left">${item.cityName} : ${item.number}</div>`
                             )
-                        return e.seriesName + "<br />" + e.name + "：" + e.value + "<br/>"+"<div style='text-align: left;margin-top: 10px'>其中</div>"+ hData.join('')
+                        return e.seriesName + "<br />" + e.name + "：" + e.value + "<br/>"+"<div style='text-align: left;margin-top: 10px'>其中</div>"+ $_hData.join('')
                     }
                 }).catch(error => {
+                    console.log(error)
+                    this.$message({
+                        type: "error",
+                        message: "Unknown Error"
+                    })
                 })
             },
             getNews(){
